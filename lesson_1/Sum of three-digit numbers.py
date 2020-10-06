8ef145d25a077adace2d8aa5dc5f8f90dcbfc0a4
javinn <javid.mehdizade@gmail.com>
2020-10-05 23:51:09 -0400

    Homework 1.1

--- lesson_1/Sum of three-digit numbers.py
+++ lesson_1/Sum of three-digit numbers.py
@@ -3,9 +3,9 @@
 
 from random import randint
 
-n = int(input('Input a number of digits')) # 2
+n = int(input('Input a number of digits: ')) # 2
 
-number = randint()
+# number = randint()
 
 # n = randint(100, 999)
 # print(n)
@@ -15,23 +15,39 @@ number = randint()
 #     sum_result += int(digit_str)
 #
 # print(sum_result)
-
-once = n % 10
-# print(once)
-
-n = n // 10
-# print(n)
-
-tens = n % 10
-# print(tens)
-
-n = n // 10
-# print(n)
-
-result = n + tens + once
-
-print(result)
+#
+# once = n % 10
+# # print(once)
+#
+# n = n // 10
+# # print(n)
+#
+# tens = n % 10
+# # print(tens)
+#
+# n = n // 10
+# # print(n)
+#
+# result = n + tens + once
+#
+# print(result)
 
 # TODO: Homework: Rewrite a program with any number of digits.
 #  Instead of  3 digits, you should sum digits up from n digits number,
 #  Where  User enter n manually. n > 0
+number = randint(pow(10, n-1), pow(10, n) - 1)
+print(number)
+result = 0
+
+# # Option 1
+# for digits in str(number):
+#     result += int(digits)
+
+# Option 2
+for ind in range(1, n):
+    result += number % 10
+    number //= 10
+
+result += number
+
+print(result)
